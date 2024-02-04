1f5254161f948de82e638d609a93f1547147d570
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-02-05 00:39:33 +0100

    add projectiles_on_impact support

--- mod_root/mods-unpacked/bene_labs-WeaponDpsDisplay/extensions/weapons/weapon_stats/weapon_stats.gd
+++ mod_root/mods-unpacked/bene_labs-WeaponDpsDisplay/extensions/weapons/weapon_stats/weapon_stats.gd
@@ -3,16 +3,20 @@ extends "res://weapons/weapon_stats/weapon_stats.gd"
 
 var pierces_on_crit = 0
 var bounces_on_crit = 0
+var projectiles_on_impact_stats = null
+var atk_speed_override = null
 
 
 func set_effects(effects : Array):
 	for effect in effects:
-		match effect.key:
+		match effect.key.to_lower():
 			"effect_pierce_on_crit":
 				pierces_on_crit = effect.value
 			"effect_bounce_on_crit":
 				bounces_on_crit = effect.value
-	print(bounces_on_crit)
+			"effect_projectiles_on_hit", "effect_slow_projectiles_on_hit":
+				projectiles_on_impact_stats = effect.weapon_stats
+				projectiles_on_impact_stats.nb_projectiles = effect.value
 
 
 func get_text(base_stats: Resource) -> String:
@@ -23,10 +27,9 @@ func get_text(base_stats: Resource) -> String:
 			get_dps_text(base_dps)])
 	
 	var burn_dps = get_burning_dps(self)
-	if burn_dps <= 0:
-		return text
-	text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("WEAPON_BURN_DPS") + col_b, \
-			get_signed_col_a(1, 0) + str(burn_dps) + col_b])
+	if burn_dps > 0:
+		text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("WEAPON_BURN_DPS") + col_b, \
+				get_signed_col_a(1, 0) + str(burn_dps) + col_b])
 	return text
 
 
@@ -44,23 +47,42 @@ func get_dps_text(base_dps : float) -> String:
 		difference_str = "+" + str(stepify((dps - base_dps) / base_dps * 100, 0.01))
 	else:
 		difference_str = "-" + str(stepify((base_dps - dps) / dps * 100, 0.01))
+		difference_str = "-" + str(stepify((base_dps - dps) / dps * 100, 0.01))
 	var text = a + str(dps) + col_b
 	if (dps != base_dps):
 		text += get_init_a() + str(base_dps) + col_b
 	text += " (" + a + difference_str + "%" + col_b + ")"  
+
 	return text
 
 
-func get_dps() -> float:
-	var atk_speed =  get_cooldown_value(self)
+func get_dps(extra_damage : int = 0) -> float:
+	var atk_speed = atk_speed_override if atk_speed_override else get_cooldown_value(self)
+	damage += extra_damage
 	
-	return stepify(get_average_damage(self) / atk_speed, 0.01)
+	if additional_cooldown_every_x_shots > 0:
+		atk_speed = (atk_speed * (additional_cooldown_every_x_shots - 1.0) + \
+				atk_speed * additional_cooldown_multiplier) / additional_cooldown_every_x_shots
+	var dps = stepify(get_average_damage(self) / atk_speed, 0.01)
+	if projectiles_on_impact_stats != null:
+		var scale_damage = WeaponService.get_scaling_stats_value(projectiles_on_impact_stats.scaling_stats)
+		projectiles_on_impact_stats.atk_speed_override = get_cooldown_value(self)
+		dps += projectiles_on_impact_stats.get_dps(scale_damage)
+	damage -= extra_damage
+	return dps
 
 
 func get_base_dps(base_stats: Resource) -> float:
-	var atk_speed =  get_base_cooldown_value(base_stats)
+	var atk_speed = atk_speed_override if atk_speed_override else get_cooldown_value(base_stats)
+	if additional_cooldown_every_x_shots > 0:
+		atk_speed = (atk_speed * (additional_cooldown_every_x_shots - 1.0) + \
+				atk_speed * additional_cooldown_multiplier) / additional_cooldown_every_x_shots
 	
-	return stepify(get_average_damage(base_stats) / atk_speed, 0.01)
+	var base_dps = stepify(get_average_damage(base_stats) / atk_speed, 0.01)
+	if projectiles_on_impact_stats != null:
+		projectiles_on_impact_stats.atk_speed_override = get_cooldown_value(base_stats)
+		base_dps += projectiles_on_impact_stats.get_dps()
+	return base_dps
 
 
 func get_average_damage(stats: Resource) -> float:
