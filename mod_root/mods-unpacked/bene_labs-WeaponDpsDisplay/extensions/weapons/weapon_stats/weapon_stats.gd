c280d524802867c08208602d4385d1d722f7c068
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-02-19 20:46:27 +0100

    mod burn dps, add burn dps per stack, fix dps comparison

--- mod_root/mods-unpacked/bene_labs-WeaponDpsDisplay/extensions/weapons/weapon_stats/weapon_stats.gd
+++ mod_root/mods-unpacked/bene_labs-WeaponDpsDisplay/extensions/weapons/weapon_stats/weapon_stats.gd
@@ -26,10 +26,13 @@ func get_text(base_stats: Resource) -> String:
 	text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("WEAPON_DPS") + col_b, \
 			get_dps_text(base_dps)])
 	
-	var burn_dps = get_burning_dps(self)
-	if burn_dps > 0:
-		text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("WEAPON_BURN_DPS") + col_b, \
-				get_signed_col_a(1, 0) + str(burn_dps) + col_b])
+	var burn_dps_per_stack = get_burning_dps_pet_stack(self)
+	var max_burn_dps = get_max_burning_dps(self)
+	if burn_dps_per_stack > 0:
+		text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("BURN_STACK_DPS") + col_b, \
+				get_signed_col_a(1, 0) + str(burn_dps_per_stack) + col_b])
+		text += "\n" + Text.text("STAT_FORMATTED", [get_col_a() + tr("BURN_MAX_DPS") + col_b, \
+				get_signed_col_a(1, 0) + str(max_burn_dps) + col_b])
 	return text
 
 
@@ -42,31 +45,32 @@ func get_base_dps_text(base_stats: Resource) -> String:
 func get_dps_text(base_dps : float) -> String:
 	var dps = get_dps()
 	var a = get_signed_col_a(dps, base_dps)
-	var difference_str = ""
-	if dps >= base_dps:
-		difference_str = "+" + str(stepify((dps - base_dps) / base_dps * 100, 0.01))
-	else:
-		difference_str = "-" + str(stepify((base_dps - dps) / dps * 100, 0.01))
-		difference_str = "-" + str(stepify((base_dps - dps) / dps * 100, 0.01))
+	var difference_str = ("+" if dps > base_dps else "") + \
+			str(stepify((dps - base_dps) / base_dps * 100, 0.01))
 	var text = a + str(dps) + col_b
-	if (dps != base_dps):
+	if dps != base_dps:
 		text += get_init_a() + str(base_dps) + col_b
-	text += " (" + a + difference_str + "%" + col_b + ")"  
+		text += " (" + a + difference_str + "%" + col_b + ")"  
 
 	return text
 
 
-func get_dps(extra_damage : int = 0) -> float:
-	var atk_speed = atk_speed_override if atk_speed_override else get_cooldown_value(self)
-	damage += extra_damage
-	
-	if additional_cooldown_every_x_shots > 0:
-		var additional_cooldown_stats = self.duplicate()
-		additional_cooldown_stats.cooldown *= additional_cooldown_multiplier
+func get_average_atk_speed(stats: Resource):
+	var atk_speed = stats.atk_speed_override if stats.atk_speed_override else stats.get_cooldown_value(self)
+	if stats.additional_cooldown_every_x_shots > 0:
+		var additional_cooldown_stats = stats.duplicate()
+		additional_cooldown_stats.cooldown *= stats.additional_cooldown_multiplier
 		var additional_atk_speed = additional_cooldown_stats.get_cooldown_value(additional_cooldown_stats)
-		atk_speed = (atk_speed * (additional_cooldown_every_x_shots - 1.0) + additional_atk_speed) \
-				/ additional_cooldown_every_x_shots
+		atk_speed = (atk_speed * (stats.additional_cooldown_every_x_shots - 1.0) + additional_atk_speed) \
+				/ stats.additional_cooldown_every_x_shots
+	return atk_speed
+
+
+func get_dps(extra_damage : int = 0) -> float:
+	var atk_speed = get_average_atk_speed(self)
+	damage += extra_damage
 	var dps = stepify(get_average_damage(self) / atk_speed, 0.01)
+	
 	if projectiles_on_impact_stats != null:
 		var scale_damage = WeaponService.get_scaling_stats_value(projectiles_on_impact_stats.scaling_stats)
 		projectiles_on_impact_stats.atk_speed_override = get_cooldown_value(self)
@@ -76,15 +80,9 @@ func get_dps(extra_damage : int = 0) -> float:
 
 
 func get_base_dps(base_stats: Resource) -> float:
-	var atk_speed =  get_base_cooldown_value(base_stats)
-	if base_stats.additional_cooldown_every_x_shots > 0:
-		var additional_cooldown_stats = base_stats.duplicate()
-		additional_cooldown_stats.cooldown *= additional_cooldown_multiplier
-		var additional_atk_speed = additional_cooldown_stats.get_cooldown_value(additional_cooldown_stats)
-		atk_speed = (atk_speed * (base_stats.additional_cooldown_every_x_shots - 1.0) + additional_atk_speed) \
-				/ base_stats.additional_cooldown_every_x_shots
-	
+	var atk_speed = get_average_atk_speed(base_stats)
 	var base_dps = stepify(get_average_damage(base_stats) / atk_speed, 0.01)
+
 	if projectiles_on_impact_stats != null:
 		projectiles_on_impact_stats.atk_speed_override = get_cooldown_value(base_stats)
 		base_dps += projectiles_on_impact_stats.get_dps()
@@ -118,12 +116,22 @@ func get_average_damage(stats: Resource) -> float:
 	return dmg
 
 
-func get_burning_dps(stats: Resource) -> float:
+func get_burning_dps_pet_stack(stats: Resource) -> float:
 	var burning = stats.burning_data
-	var cooldown = get_cooldown_value(stats)
-	var new_burn_damage_per_second = burning.chance * burning.damage / cooldown
+	var burn_speed = 1.0 - (RunData.effects["burning_cooldown_reduction"] / 100.0)
+	var atk_speed = get_average_atk_speed(stats)
+	var burn_damage_per_second = burning.chance * burning.damage / burn_speed
+	
+	return stepify(burn_damage_per_second, 0.01)
+
+
+func get_max_burning_dps(stats: Resource) -> float:
+	var burning = stats.burning_data
+	var burn_speed = 1.0 - (RunData.effects["burning_cooldown_reduction"] / 100.0)
+	var atk_speed = get_average_atk_speed(stats)
+	var new_burn_damage_per_second = burning.chance * burning.damage / atk_speed / burn_speed * (1 + burning.spread)
 	
 	if stats is RangedWeaponStats:
 		new_burn_damage_per_second *= stats.nb_projectiles
 	# Burn duration is accounted for as it might cause multiple stacks of burn damage to go off in the same second
-	return stepify(new_burn_damage_per_second + (burning.duration - cooldown) * new_burn_damage_per_second, 0.01)
+	return stepify(new_burn_damage_per_second + (burning.duration - atk_speed) * new_burn_damage_per_second, 0.01)
